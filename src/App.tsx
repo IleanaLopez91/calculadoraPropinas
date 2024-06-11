00d119dc843c992c3d9c1d2add3dcc5568158f01
Ileana <ferchu91garcia@gmail.com>
2024-06-11 20:56:43 -0300

    modificando codigo para hacer el deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { menuItems } from "./data/db";
 import useOrder from "./hooks/useOrder";
 
 function App() {
-  const { order, addItem, removeItem, tip, setTip } = useOrder();
+  const { order, addItem, removeItem, tip, setTip, placeOrder } = useOrder();
   return (
     <>
       <header className=" bg-teal-400 py-5">
@@ -24,9 +24,15 @@ function App() {
           </div>
         </div>
         <div className=" border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-          <OrderContent order={order} removeItem={removeItem} />
-          <TipPercentageForm setTip={setTip} />
-          <OrderTotal order={order} tip={tip} />
+          {order.length > 0 ? (
+            <>
+              <OrderContent order={order} removeItem={removeItem} />
+              <TipPercentageForm setTip={setTip} tip={tip} />
+              <OrderTotal order={order} tip={tip} placeOrder={placeOrder} />
+            </>
+          ) : (
+            <p className=" text-center">La orden esta vacia</p>
+          )}
         </div>
       </main>
     </>
--- src/components/OrderTotal.tsx
+++ src/components/OrderTotal.tsx
@@ -5,9 +5,10 @@ import { formatCurrency } from "../helpers";
 type OrderTotalProps = {
   order: OrderItem[];
   tip: number;
+  placeOrder: () => void;
 };
 
-function OrderTotal({ order, tip }: OrderTotalProps) {
+function OrderTotal({ order, tip, placeOrder }: OrderTotalProps) {
   const subTotalAmount = useMemo(
     () => order.reduce((total, item) => total + item.price * item.quantity, 0),
     [order]
@@ -34,7 +35,13 @@ function OrderTotal({ order, tip }: OrderTotalProps) {
           <span className=" font-bold">{formatCurrency(totalAmount)}</span>
         </p>
       </div>
-      <button></button>
+      <button
+        className=" w-full bg-black p-3 uppercase text-white font-bolt mt-10 disabled:opacity-10"
+        disabled={totalAmount === 0}
+        onClick={placeOrder}
+      >
+        Guardar Orden
+      </button>
     </>
   );
 }
